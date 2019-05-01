29e826a4551b9e864d4a9cf59de745399e725a54
Martin Berg <martin.yngve.berg@gmail.com>
2019-05-01 14:16:39 +0200

    .

--- Train/MainWindow.xaml.cs
+++ Train/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Train
 
         Label label;
         bool tempMenuItems;
+        public bool alreadyGrabbed = false;
 
         public MainWindow()
         {
@@ -38,7 +39,7 @@ namespace Train
         public void ChangeLabel(String change)
         {
             Label l = MainDock.Children[0] as Label;
-            l.Content = change;
+            l.Content = "Test:" + change;
         }
 
         //Method for Removing elements
@@ -132,26 +133,123 @@ namespace Train
         Point onCanvas = new Point();
         Point onPanel = new Point();
         Vector test = new Vector();
+        DockPanel parentDockPanel;
+        Canvas parentCanvas;
 
         protected override void OnMouseDown(MouseButtonEventArgs e)
         {
-            this.Background = Brushes.Aqua;
+            if (mainWindow.alreadyGrabbed == true)
+            {
+                return;
+            }
 
-            onCanvas = e.GetPosition(mainWindow.MainCanvas);
+            mainWindow.alreadyGrabbed = true;
+
+            Panel parent = this.Parent as Panel;
+
+            onCanvas = e.GetPosition(parent);
+
+            //onCanvas = e.GetPosition(mainWindow.MainCanvas);
             onPanel = e.GetPosition(this);
 
             test = Point.Subtract(onCanvas, onPanel);
 
-            mainWindow.ChangeLabel(test.ToString());
+            //mainWindow.ChangeLabel(test.ToString());
 
-            //mainWindow.RemoveObject(this);
+            if (this.Parent is DockPanel)
+            {
+                parentDockPanel = this.Parent as DockPanel;
+            }
+
+            if (this.Parent is Canvas)
+            {
+                parentCanvas = this.Parent as Canvas;
+            }
+
+            
+
+            PlaceholderGUI placeholder = new PlaceholderGUI(this.ActualWidth, this.ActualHeight);
+
+            parent.Children.Insert(parent.Children.IndexOf(this), placeholder);
+
+            
+            mainWindow.RemoveObject(this);
 
 
 
-            //mainWindow.MainCanvas.Children.Add(this);
+            Canvas.SetLeft(this, test.X);
+            Canvas.SetLeft(this, test.Y);
+
+            mainWindow.MainCanvas.Children.Add(this);
+
+            //Ok, everything done, now we can start draging
             dragStart = e.GetPosition(this);
-            //CaptureMouse();
+            CaptureMouse();
+        }
 
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            int change = 0;
+
+            dragStart = null;
+            ReleaseMouseCapture();
+
+            //Panel parent = this.Parent as Panel;
+
+            if (parentDockPanel != null)
+            {
+                mainWindow.ChangeLabel("dockpanel");
+
+                for (int i = 0; i < parentDockPanel.Children.Count; i++)
+                {
+                    if (parentDockPanel.Children[i] is PlaceholderGUI)
+                    {
+                        //change++;
+                        parentDockPanel.Children.RemoveAt(i);
+                    }
+                }
+
+                parentDockPanel = null;
+            }
+            if (parentCanvas != null)
+            {
+                mainWindow.ChangeLabel("canvas");
+
+                for (int i = 0; i < parentCanvas.Children.Count; i++)
+                {
+                    if (parentCanvas.Children[i] is PlaceholderGUI)
+                    {
+                        //change++;
+                        parentCanvas.Children.RemoveAt(i);
+                    }
+                }
+            }
+
+
+            //mainWindow.ChangeLabel(change.ToString());
+
+            mainWindow.alreadyGrabbed = false;
+
+            //List<PlaceholderGUI> removal = parent.Children.OfType<PlaceholderGUI>().ToList<PlaceholderGUI>();
+
+            //foreach (PlaceholderGUI obj in removal)
+            //{
+
+            //    mainWindow.RemoveObject(obj);
+            //}
+
+            //foreach (var obj in parent.Children)
+            //{
+            //    if (obj is PlaceholderGUI)
+            //    {
+            //        FrameworkElement frameworkElement = obj as FrameworkElement;
+
+            //        mainWindow.RemoveObject(frameworkElement);
+            //    }
+
+
+            //}
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
@@ -159,9 +257,26 @@ namespace Train
             if (dragStart != null && e.LeftButton == MouseButtonState.Pressed)
             {
                 Point p2 = e.GetPosition(mainWindow.MainCanvas);
+
+                Canvas.SetLeft(this, p2.X - dragStart.Value.X);
+                Canvas.SetTop(this, p2.Y - dragStart.Value.Y);
+
             }
 
             base.OnMouseMove(e);
+        }
+    }
+
+    public class PlaceholderGUI : PanelGUI
+    {
+        public PlaceholderGUI(double width, double height)
+        {
+            Background = Brushes.Green;
+            DockPanel.SetDock(this, Dock.Top);
+            this.Visibility = Visibility.Visible;
+
+            this.Width = width;
+            this.Height = height;
         }
     }
 
